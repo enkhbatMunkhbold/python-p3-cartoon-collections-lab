c20944d72c9a0e72e25b39c12fdce7555df9e689
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-04 16:13:44 -0700

    Complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(col):
+    for i in range(len(col)):
+        print(f"{i + 1}. {col[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(col):
+    return [(call + '!').title() for call in col]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(col):
+    for s in col:
+        if len(s) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(data):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+    for s in data:
+        if s in cheeses:
+            return s
